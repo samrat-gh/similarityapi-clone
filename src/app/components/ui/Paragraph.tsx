471c83311808f16e03fb89cb36e4a497b9830d94
samrat-gh <samratghimire32@gmail.com>
2023-04-16 20:27:51 +0545

    initial commit

---
+++ src/app/components/ui/Paragraph.tsx
@@ -0,0 +1,30 @@
+import { FC, HTMLAttributes, forwardRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+
+
+const paragraphVariants = cva(
+  "max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center",
+  {
+    variants: {
+      size: {
+        default: "text-base sm:text-lg",
+        sm: "text-sm, sm:text-base",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  }
+);
+
+interface ParagraphProps extends HTMLAttributes<HTMLParagraphElement>, VariantProps<typeof paragraphVariants> {}
+
+const Paragraph: FC<ParagraphProps> = ({}) => {
+  return <div> Paragraph</div>;
+};
+
+
+// const Paragraph = forwardRef(({})) => {
+//   return <div> Paragraph </div>})
+
+export default Paragraph;
